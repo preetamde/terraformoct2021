a4e0e1e1c6e85b931d9ea4634ce8dea7baeb44d8
preetamde <github@preetamzare.com>
2021-10-31 12:04:16 +0100

    moved the files

--- Stack/Web/vpc_dev/webserver.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-sudo apt-get update
-sudo apt-get install -y apache2
-sudo chown -R ubuntu:ubuntu /var/www/html/
-sudo touch /var/www/html/healthcheck.html
-sudo touch /var/www/html/index.html
-sudo echo "<h1 align = center> Instance : $(uname -n) $(date) </h1>" >/var/www/html/index.html
